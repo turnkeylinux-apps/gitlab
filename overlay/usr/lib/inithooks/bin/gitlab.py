198c94544abb2c8c1c711224275a0305b4eb66ed
Anton Pyrogovskyi <anton@turnkeylinux.org>
2022-12-17 21:53:50 +0100

    update gitlab inithook

--- overlay/usr/lib/inithooks/bin/gitlab.py
+++ overlay/usr/lib/inithooks/bin/gitlab.py
@@ -14,7 +14,7 @@ import getopt
 from libinithooks import inithooks_cache
 import os
 import pwd
-import subprocess
+from subprocess import run, Popen, PIPE
 
 from libinithooks.dialog_wrapper import Dialog
 
@@ -82,41 +82,19 @@ def main():
 
     inithooks_cache.write('APP_DOMAIN', domain)
     
-    print("Reconfiguring GitLab. This might take a while. Please wait.")
-
+    print("Reconfiguring GitLab. This might take a while.")
     config = "/etc/gitlab/gitlab.rb"
     domain = "http://%s" % domain
-    subprocess.run(["sed", "-i", "/^external_url/ s|'.*|'%s'|" % domain, config])
-    subprocess.run(["sed", "-i", "/^gitlab_rails\['gitlab_email_from'\]/ s|=.*|= '%s'|" % email, config])
-    subprocess.run(["gitlab-ctl", "reconfigure"])
+    run(["sed", "-i", "/^external_url/ s|'.*|'%s'|" % domain, config])
+    run(["sed", "-i", "/^gitlab_rails\['gitlab_email_from'\]/ s|=.*|= '%s'|" % email, config])
+    run(["gitlab-ctl", "reconfigure"])
 
-    print("Setting GitLab 'root' user password and email in database. This might take a while too. Please wait (again).")
-    tmp_dir = '/run/user/0'
-    tmp_file = '.gitlab-init.rb'
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
-    tmp_path = '/'.join([tmp_dir, tmp_file])
-    # include token resetting here now (just before 'exit'); should fix #1315/#1342 for good!
-    tmp_contents = """
-        ActiveRecord::Base.logger.level = 1
-        u = User.where(id: 1).first
-        u.password = u.password_confirmation = '{}'
-        u.email = '{}'
-        u.skip_reconfirmation!
-        u.save!
-        ApplicationSetting.current.reset_runners_registration_token!
-        exit
-    """
-    flags = os.O_WRONLY | os.O_CREAT
-    with os.fdopen(os.open(tmp_path, flags, 0o600), 'w') as fob:
-        fob.write(tmp_contents.format(password, email))
-    uid = pwd.getpwnam('git').pw_uid
-    os.chown(tmp_path, uid, 0)
-    try:
-        subprocess.run(["gitlab-rails", "runner", "-e", "production", tmp_path])
-        print("Done.")
-    finally:
-        os.remove(tmp_path)
+    print("Setting GitLab 'root' user password. This might take a while.")
+    p1 = Popen(["echo", "-e", "{}\n{}\n".format(password, password)], stdout=PIPE)
+    p2 = Popen(["gitlab-rake", "gitlab:password:reset[root]"], stdin=p1.stdout, stdout=PIPE)
+    p1.stdout.close()
+    output = p2.communicate()[0]
+    print(output)
 
 if __name__ == "__main__":
     main()
